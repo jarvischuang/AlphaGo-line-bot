a12cdb64cc90b3247e7b468923e578b84a35bc8d
jarvis <jarvis@universetech.cc>
2022-12-21 10:00:52 +0800

    toLowerCase

--- app.js
+++ app.js
@@ -45,7 +45,7 @@ async function handleEvent(event) {
   // create a echoing text message
   const echo = { type: 'text', text: completion.data.choices[0].text.trim() };
 
-  if (event.message.text.startsWith('@Jarvis Bot ')) {
+  if (event.message.text.toLowerCase().startsWith('@jarvis bot ')) {
     // use reply API
     return client.replyMessage(event.replyToken, echo);
   } else {
